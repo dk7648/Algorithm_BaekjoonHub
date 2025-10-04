5f14b6c978603fb35e2405b96306ad8a599c32a1
dk7648 <76927860+dk7648@users.noreply.github.com>
2025-10-04 10:24:18 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 237 ms, Memory: 53,060 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,10 +1,8 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 import java.io.FileInputStream;
-import java.util.Stack;
 import java.awt.Point;
 
 class Solution
@@ -33,18 +31,18 @@ class Solution
                 }
             }
             dist[0][0] = 0;
-            Stack<Point> stack = new Stack<>();
-            stack.push(new Point(0, 0));
+            Queue<Point> queue = new LinkedList<>();
+            queue.offer(new Point(0, 0));
 
-            while(!stack.isEmpty()) {
-                Point target = stack.pop();
+            while(!queue.isEmpty()) {
+                Point target = queue.poll();
                 for(int i=0; i<4; i++) {
                     int nx = target.x+dx[i];
                     int ny = target.y+dy[i];
                     if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
                     if(dist[ny][nx] <= dist[target.y][target.x] + map[ny][nx]) continue;
                     dist[ny][nx] = dist[target.y][target.x] + map[ny][nx];
-                    stack.push(new Point(nx, ny));
+                    queue.offer(new Point(nx, ny));
                 }
             }
             System.out.println("#"+test_case+" "+dist[n-1][n-1]);
