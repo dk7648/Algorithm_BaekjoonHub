131999af95325162a9a14792df5e245faf081b99
dk7648 <76927860+dk7648@users.noreply.github.com>
2025-12-03 13:09:22 +0900

    [Gold V] Title: 토마토, Time: 736 ms, Memory: 146284 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,63 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-class Main {
-    private static class Pos {
-        public int x;
-        public int y;
-        public Pos(int y, int x) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    public static void main(String[] args) throws IOException {
+class Main
+{
+    static int[] dy = {-1,0,1,0};
+    static int[] dx = {0,1,0,-1};
+
+    public static void main(String args[]) throws Exception
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Queue<Pos> q = new LinkedList<>();
-        int[] dy = {-1,0,1,0};
-        int[] dx = {0,1,0,-1};
-        
-        int[] data = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int w = data[0];
-        int h = data[1];
-        
-        int[][] map = new int[h][w];
-        int[][] dist = new int[h][w];
-        for(int i=0; i<h; i++) {
-            data = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            for(int k=0; k<w; k++) {
-                map[i][k] = data[k];
-                if(data[k] == 1) {
-                    q.add(new Pos(i,k));
-                }
-                if(data[k] == 0) {
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[1]);
+        int M = Integer.parseInt(input[0]);
+        String[][] map = new String[N][M];
+        int[][] dist = new int[N][M];
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0; i<N; i++) {
+            map[i] = br.readLine().split(" ");
+            for(int k=0; k<M; k++) {
+                if(map[i][k].equals("1")) {
+                    dist[i][k] = 0;
+                    q.add(new int[]{i,k});
+                } else if(map[i][k].equals("0")) {
                     dist[i][k] = -1;
                 }
             }
         }
-    
+
         while(!q.isEmpty()) {
-            Pos target = q.poll();
+            int[] target=  q.poll();
             for(int i=0; i<4; i++) {
-                int nx = target.x+dx[i];
-                int ny = target.y+dy[i];
-                if(nx <0 || nx >= w || ny < 0 || ny >= h) continue;
+                int ny = target[0]+dy[i];
+                int nx = target[1]+dx[i];
+                if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
                 if(dist[ny][nx] >= 0) continue;
-                q.add(new Pos(ny, nx));
-                dist[ny][nx] = dist[target.y][target.x] + 1;
+                q.add(new int[]{ny,nx});
+                dist[ny][nx] = dist[target[0]][target[1]] + 1;
+
             }
         }
-
         int max = 0;
-        for(int i=0; i<h; i++) {
-            for(int k=0; k<w; k++) {
+        for(int i=0; i<N; i++) {
+            for(int k=0;k<M; k++) {
                 if(dist[i][k] == -1) {
-                    System.out.println(-1);
-                    return;
+                    System.out.println(-1); return;
                 }
                 max = Math.max(max, dist[i][k]);
             }
         }
         System.out.println(max);
-        return;
     }
 }
