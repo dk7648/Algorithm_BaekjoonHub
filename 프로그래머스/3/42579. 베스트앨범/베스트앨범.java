a328f28b7805193a2050d3dc2a96e379f42f4b6d
dk7648 <76927860+dk7648@users.noreply.github.com>
2025-12-11 13:41:24 +0900

    [level 3] Title: 베스트앨범, Time: 2.01 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,48 +2,38 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int n = genres.length;
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> totalRank = new HashMap<>();
+        Map<String, List<int[]>> detailRank = new HashMap<>();
         for(int i=0; i<n; i++) {
-            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
+            totalRank.put(genres[i], totalRank.getOrDefault(genres[i], 0) + plays[i]);
+            
+            List<int[]> list = detailRank.getOrDefault(genres[i], new ArrayList<>());
+            list.add(new int[]{i, plays[i]});      
+            detailRank.put(genres[i], list);
         }
+        List<String> genreRank = new ArrayList<>();
+        genreRank.addAll(totalRank.keySet());
+        genreRank.sort((a,b)->totalRank.get(b) - totalRank.get(a));
         
-        Map<Integer, Integer> map2 = new HashMap<>();
-        for(int i=0; i<n; i++) {
-            map2.put(i, plays[i]);
-        }
-        
-        List<Integer> result = new LinkedList<>();
-        List<String> sortedGenres = sortedByValue(map);
-        List<Integer> sortedIndexes = sortedByValue(map2);
-        
-        for (String key : sortedGenres) {
-            int count = 0;
-            for(int i=0; i<n; i++) {
-                int index = sortedIndexes.get(i);
-                if(genres[index].equals(key)) {
-                    result.add(index);
-                    count++;
-                }
-                if(count >= 2) break;
+        List<Integer> result = new ArrayList<>();
+        for(String genre : genreRank) {
+            List<int[]> list = detailRank.get(genre);
+            list.sort((a,b)-> {
+                if(a[1] == b[1]) return a[0] - b[0];
+                return b[1] - a[1];
+            });
+            
+            result.add(list.get(0)[0]);
+            if(list.size() > 1) {
+                result.add(list.get(1)[0]);
             }
-        }
-
+        }        
         int[] answer = new int[result.size()];
+        
         for(int i=0; i<result.size(); i++) {
             answer[i] = result.get(i);
         }
-        return answer;
-    }
-    
-    public ArrayList sortedByValue(final Map map) {
-        ArrayList<Object> list = new ArrayList<>();
-        list.addAll(map.keySet());
         
-        list.sort((o1, o2) -> {
-            Object v1 = map.get(o1);
-            Object v2 = map.get(o2);
-            return ((Comparable) v2).compareTo(v1);
-        });
-        return list;
+        return answer;
     }
 }
