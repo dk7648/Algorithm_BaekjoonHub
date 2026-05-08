7c0739dd065a5fe606d3332d51f22705faba2b81
dk7648 <76927860+dk7648@users.noreply.github.com>
2026-05-09 08:09:48 +0900

    [level 2] Title: 더 맵게, Time: 933.78 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> a-b);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int s : scoville) {
             pq.add(s);
         }
